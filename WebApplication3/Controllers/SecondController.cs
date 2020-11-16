02b5a1ecf3dee9bd20e2c5b1d01551490620cb7e
“othmanedendane” <othmanedendane99@gmail.com>
2020-11-16 11:22:56 -0500

    “test”

---
+++ WebApplication3/Controllers/SecondController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class SecondController : Controller
+    {
+        // GET: Second
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+     
+        public ActionResult Succes()
+        {
+            return View();
+        }
+        public ActionResult Fail()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Insert(WebApplication3.Models.Class1 li)
+        {
+
+
+            return View(li);
+        }
+
+        public ActionResult InsertData(WebApplication3.Models.Class1 li)
+        {
+            if (ModelState.IsValid)
+            {
+                WebApplication3.Models.Enterwhatever en = new Models.Enterwhatever();
+                en.InsertData(li);
+                //  ViewBag.name = li.nom;
+
+
+                try
+                {
+                    if (ModelState.IsValid)
+                    {
+                        var senderEmail = new MailAddress("funfunny573@gmail.com", "Othmane.D");
+                        var receiverEmail = new MailAddress(li.nom, "Receiver");
+                        var passwordd = "dimaraja";
+                        var body = "Votre Username est : " + li.login + "  et Votre Password est : " + li.pw;
+                        var smtp = new SmtpClient
+                        {
+                            Host = "smtp.gmail.com",
+                            Port = 587,
+                            EnableSsl = true,
+                            DeliveryMethod = SmtpDeliveryMethod.Network,
+                            UseDefaultCredentials = false,
+                            Credentials = new NetworkCredential(senderEmail.Address, passwordd)
+                        };
+                        using (var mess = new MailMessage(senderEmail, receiverEmail)
+                        {
+                            Subject = "Personal first Login Infos",
+                            Body = body
+
+                        })
+                        {
+                            smtp.Send(mess);
+                        }
+                        return View();
+                    }
+                }
+                catch (Exception)
+                {
+                    ViewBag.Error = "Some Error";
+                }
+
+
+
+
+                @ViewBag.data = "Data inserted successfully !";
+                return View("Insert");
+            }
+            else
+            {
+                return View("Insert");
+            }
+
+
+
+        }
+
+
+        public ActionResult Loginn(WebApplication3.Models.Class1 li)
+        {
+            return View(li);
+
+        }
+        public ActionResult Loginsearch(WebApplication3.Models.Class1 li)
+        {
+            WebApplication3.Models.Search ss = new Models.Search();
+            string pass = ss.searchLP(li);
+
+
+            if (pass == li.pw)
+            {
+
+                return View("Succes");
+
+            }
+            @ViewBag.data = "invalide Login (Plz try again...)";
+            return View("Fail");
+
+        }
+
+
+
+
+
+    }
+}
---
+++ WebApplication3/Models/Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Class1
+    {
+        [Column(IsPrimaryKey = true)]
+        public int Id { get; set; }
+
+        [Required]
+        public string nom { get; set; }
+        [Required]
+        
+        public string prenom { get; set; }
+
+        [Required]
+
+        public string age { get; set; }
+
+    
+
+        public string login { get; set; }
+
+     
+
+        public string pw { get; set; }
+    }
+
+
+    public class Enterwhatever
+    {
+        public void InsertData(Class1 li)
+    {
+
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        perso rs = new perso();
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringCharsuser = new char[6];
+            var stringCharspass = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringCharsuser.Length; i++)
+            {
+                stringCharsuser[i] = chars[random.Next(chars.Length)];
+            }
+            for (int i = 0; i < stringCharspass.Length; i++)
+            {
+                stringCharspass[i] = chars[random.Next(chars.Length)];
+            }
+
+
+            li.login = new String(stringCharsuser);
+            li.pw = new String(stringCharspass);
+
+
+            rs.Id = li.Id;
+        rs.nom = li.nom;
+        rs.prenom = li.prenom;
+        rs.age = li.age;
+        rs.login = li.login;
+        rs.pw = li.pw;
+
+
+            db.perso.InsertOnSubmit(rs);
+        db.SubmitChanges();
+
+    }
+
+    }
+
+
+
+    public class Search
+    {
+
+        public string searchLP(Class1 li)
+        {
+
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            perso rs = new perso();
+            string passout = "";
+            // var pass = from m in db.registers where m.emailid == li.Emailid select m.userpassword;  
+            var pass = from m in db.perso where m.login == li.login select m.pw;
+            // var pass = from m in db.registers where m.emailid == li.Emailid select m.userpassword;
+            foreach (string query in pass)
+            {
+                passout = query;
+
+            }
+            return passout;
+
+        }
+
+    }
+
+}
